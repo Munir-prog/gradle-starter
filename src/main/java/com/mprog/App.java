3574f8da69cd7bf39729bbf2c3dc281527e4df47
Munir Zikrullaev <munirprog@gmail.com>
2021-11-27 12:06:32 +0300

    Jar Task 1 part

--- src/main/java/com/mprog/App.java
+++ src/main/java/com/mprog/App.java
@@ -1,7 +1,10 @@
 package com.mprog;
 
+import org.springframework.util.StringUtils;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+
+        System.out.println(StringUtils.capitalize("hello world"));
     }
 }
