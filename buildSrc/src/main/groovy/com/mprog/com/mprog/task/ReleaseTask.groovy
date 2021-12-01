000cdf57539f5ab507e5e40fd8043f702ceb37ce
Munir Zikrullaev <munirprog@gmail.com>
2021-12-01 20:26:55 +0300

    Task cache

---
+++ buildSrc/src/main/groovy/com/mprog/com/mprog/task/ReleaseTask.groovy
@@ -0,0 +1,29 @@
+package com.mprog.com.mprog.task
+
+import org.gradle.api.DefaultTask
+import org.gradle.api.tasks.Input
+import org.gradle.api.tasks.OutputFile
+import org.gradle.api.tasks.TaskAction
+
+class ReleaseTask extends DefaultTask {
+
+    @Input
+    boolean release
+
+    @OutputFile
+    File releaseFile
+
+    @TaskAction
+    void performRelease(){
+        println "release in progress"
+    }
+
+
+
+//    inputs.property ( "release", project.release )
+//    outputs.file ( " $ rootDir /script.gradle " )
+//
+//    doLast {
+//        println "release in progress..."
+//    }
+}
